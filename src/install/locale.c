51b37a9d204606b787e6a7b668aca2c31a2c6e2e
MOHAPY24 <mohammedmahmoud123b@gmail.com>
2026-01-04 02:22:31 +0400

    locale configuring

--- src/install/locale.c
+++ src/install/locale.c
@@ -2,8 +2,13 @@
 
 int configure_locale(void)
 {
-    // TODO: Implement locale configuration logic.
-    (void)get_store();
-    napms(200);
+    Store store = get_store();
+    char *const args = ["sed"];
+    char cmd[256];
+    sprintf(cmd, "sed -i '/^# %s/s/^# //' /etc/locale.gen; locale-gen 2> /dev/null", store.locale);
+    run_cmd_chroot(cmd, args);
+    sprintf(cmd, "echo LANG='%s' > /etc/default/locale", store.loacle);
+    args = ["echo"];
+    run_cmd_chroot(cmd, args);
     return 0;
 }
